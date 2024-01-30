4d759246cd483361034d3e27aae0676f650ba42d
maddealer <forento@gmail.com>
2024-01-30 18:29:01 +0200

    menu done

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -4,7 +4,24 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <div className={styles.container}>
-      Home Page <Link to="/about">To About</Link>
+      <div className={styles.sloganTitle}>
+        <div className={styles.slogan}>
+          <p style={{ marginRight: "10px" }}>
+            <span className={styles.sloganL}> It </span>{" "}
+            <span className={styles.sloganR}> works on my machine :) </span>
+          </p>{" "}
+        </div>
+      </div>
+      <p className={styles.projectHeader}> Projects</p>
+      <div className={styles.projectsCards}>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+      </div>
+      <p className={styles.slogan2}>
+        It’s remarkable how a simple solution has a great impact. The hard part
+        is discovering the solution.
+      </p>
     </div>
   );
 };
--- portfolio/src/components/Menu.jsx
+++ portfolio/src/components/Menu.jsx
@@ -1,42 +1,48 @@
-// import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "./Menu.css"; // You can define your styles in a separate CSS file
 import IMAGES from "../images/IMAGES";
-import styles from "./Menu.module.css";
-import navStyles from "./MenuNav.module.css";
-const Menu = () => {
-  return (
-    <div className={styles.container}>
-      <header className={navStyles.header}>
-        <Link to="/" className={navStyles.logo}>
-          <img
-            className={navStyles.imgsize}
-            // style={{ width: "100%", height: "auto" }}
 
-            src={IMAGES.image1}
-            alt="first image"
-            width={80}
-            height={80}
-          />{" "}
-        </Link>{" "}
-        <input
-          className={navStyles["menu-btn"]}
-          type="checkbox"
-          id="menu-btn"
-        />
-        <label className={navStyles["menu-icon"]} htmlFor="menu-btn">
-          <span className={navStyles.navicon}></span>
-        </label>{" "}
-        <ul className={navStyles.menu}>
-          <li>
-            <Link to="/">PORTFOLIO</Link>
-          </li>
-          <li>
-            <Link to="/about">ABOUT</Link>
-          </li>
+const HamburgerMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="header">
+      <div className="logo">
+        {" "}
+        <img
+          src={IMAGES.image1}
+          alt="first image"
+          width={50}
+          height={50}
+        />{" "}
+      </div>
+      <div className="title">Hristomir H Portfolio</div>
+      <div
+        className={`menu-icon ${isMenuOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        {isMenuOpen ? (
+          <span style={{ fontSize: "1rem", fontWeight: "bold" }}>X</span>
+        ) : (
+          <>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </>
+        )}
+      </div>
+      <div className={`menu ${isMenuOpen ? "open" : ""}`}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
         </ul>
-      </header>
+      </div>
     </div>
   );
 };
 
-export default Menu;
+export default HamburgerMenu;
